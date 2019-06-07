76bdc8be67b86155314f1727a968a8d97fd7672a
mySuulola <oluwaseyisuulola@gmail.com>
2019-06-07 09:14:04 +0100

    need to read up first on firebase function

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Notifications = () => {
   return (
     <div>
-      <p>Notifications</p>
+      <div className=" mb-4 bg-light" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Notifications</h5>
+          <p class="card-text">Notifications here.</p>
+          <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+         
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Notifications
+export default Notifications;
