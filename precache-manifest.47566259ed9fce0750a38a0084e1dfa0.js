9510196e9384c9f2cfa833dab031d3c9ae7fba72
mySuulola <oluwaseyisuulola@gmail.com>
2019-06-17 12:00:59 +0100

    Updates

---
+++ precache-manifest.47566259ed9fce0750a38a0084e1dfa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee3d35acfb714ee1cec2",
+    "url": "/marioplan/static/js/runtime~main.ee3d35ac.js"
+  },
+  {
+    "revision": "d2e90ad7f73a1faf5c76",
+    "url": "/marioplan/static/js/main.d2e90ad7.chunk.js"
+  },
+  {
+    "revision": "c50c0538b488d7fc6847",
+    "url": "/marioplan/static/js/2.c50c0538.chunk.js"
+  },
+  {
+    "revision": "d2e90ad7f73a1faf5c76",
+    "url": "/marioplan/static/css/main.54e32970.chunk.css"
+  },
+  {
+    "revision": "505afac094ec19768dfa6846af665cfa",
+    "url": "/marioplan/index.html"
+  }
+];
