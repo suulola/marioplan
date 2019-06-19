138ea5edb389d9735d0e709e9900e9f7fef8825e
mySuulola <oluwaseyisuulola@gmail.com>
2019-06-19 13:32:24 +0100

    deploy to firebase at https://react-marioplan-188d8.firebaseapp.com

---
+++ dist/precache-manifest.fb71ebde761ddd2ba0f91b942a10a165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e286635728b720585eeb",
+    "url": "/static/js/main.e2866357.chunk.js"
+  },
+  {
+    "revision": "dade53ae737d9d7fe2a8",
+    "url": "/static/js/2.dade53ae.chunk.js"
+  },
+  {
+    "revision": "e286635728b720585eeb",
+    "url": "/static/css/main.54e32970.chunk.css"
+  },
+  {
+    "revision": "a32318542115cdb17668936ca856bd6f",
+    "url": "/index.html"
+  }
+];
